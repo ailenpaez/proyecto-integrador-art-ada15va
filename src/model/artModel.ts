d9dddfdeee6425d5f78528fe04a6d2f32ea6d0f4
ailenpaez <a.jorgelinapaez@gmail.com>
2024-01-30 14:42:42 -0300

    Uso de dependencia 'dotenv' para probar el enlace de la api alojada en '.env'.

---
+++ src/model/artModel.ts
@@ -0,0 +1,18 @@
+// ● Todas tendrán que tener un método que obtenga la data que puede llamarse:
+// getAllData().
+// ● getAllData() se utilizará dentro de todos los métodos.
+// 1. getAllTitlesArts() - Retorna un array de strings con solo los títulos de todas las
+// obras.
+// 2. getAllMappedArts() - Retorna un array con todos los trabajos pero solo mostrando:
+// id, title, department_title y dimensions en cada objeto.
+// 3. getPublicationHistoryById(Id) - Retorna un string con la publicación histórica de la
+// obra correspondiente al ID pasado por parámetro.
+// 4. getDateDisplayById(id) - Retorna un string con la fecha indicada correspondiente
+// al ID pasado por parámetro.
+
+import dotenv from "dotenv";
+dotenv.config()
+
+const urlApi = process.env.BASE_URL_API;
+
+console.log(urlApi);
