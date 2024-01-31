89154d91136b71e968486423667a41f476204a2f
ailenpaez <a.jorgelinapaez@gmail.com>
2024-01-31 14:35:49 -0300

    Método getAllData() funcionando correctamente con fetch/async/await y manejo de errores

--- src/model/artModel.ts
+++ src/model/artModel.ts
@@ -1,6 +1,33 @@
 // ● Todas tendrán que tener un método que obtenga la data que puede llamarse:
 // getAllData().
 // ● getAllData() se utilizará dentro de todos los métodos.
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const urlApi = process.env.BASE_URL_API!;
+const api = new URL(urlApi);
+
+const getAllData = async () => {
+  try {
+    const response = await fetch(api);
+
+    if (response.status === 404) {
+      throw new Error("ITEM_NOT_FOUND");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+const main = async () => {
+  const data = await getAllData();
+  console.log(data);
+};
+main();
+
 // 1. getAllTitlesArts() - Retorna un array de strings con solo los títulos de todas las
 // obras.
 // 2. getAllMappedArts() - Retorna un array con todos los trabajos pero solo mostrando:
@@ -9,19 +36,3 @@
 // obra correspondiente al ID pasado por parámetro.
 // 4. getDateDisplayById(id) - Retorna un string con la fecha indicada correspondiente
 // al ID pasado por parámetro.
-
-import dotenv from "dotenv";
-dotenv.config();
-
-const urlApi = process.env.BASE_URL_API;
-const art = process.argv[2];
-const urlParams = new URL(`${urlApi}`);
-
-const getAllData = async () => {
-  const response = fetch(urlParams);
-  const data = (await response).json();
-  const result = await data;
-  console.log(result);
-};
-
-getAllData();
